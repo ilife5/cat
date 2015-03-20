b0ab9a5d7599911d9b62a7900f44a034de631d44
ilife5 <ilife5@qq.com>
2015-03-20 17:10:55 +0800

    extract getArgument function

--- src/code/utils/log.js
+++ src/code/utils/log.js
@@ -8,15 +8,21 @@ info = chalk.cyan;
 warn = chalk.yellow;
 error = chalk.red;
 
+/**
+ * convert arguments object to array
+ */
+function getArguments(ar) {
+    return Array.prototype.slice.call( ar , 0 );
+}
 
 module.exports = {
     info: function() {
-        console.log.apply(null, [info("[modules-cat info]"), " "].concat(Array.prototype.slice.call( arguments , 0 )))
+        console.log.apply(null, [info("[modules-cat info]"), " "].concat( getArguments(arguments) ))
     },
     warn: function(msg) {
-        console.log.apply(null, [warn("[modules-cat warn]"), " "].concat(Array.prototype.slice.call( arguments , 0 )))
+        console.log.apply(null, [warn("[modules-cat warn]"), " "].concat( getArguments(arguments) ))
     },
     error: function(msg) {
-        console.log.apply(null, [error("[modules-cat error]"), " "].concat(Array.prototype.slice.call( arguments , 0 )))
+        console.log.apply(null, [error("[modules-cat error]"), " "].concat( getArguments(arguments) ))
     }
 }
